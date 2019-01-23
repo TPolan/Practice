98559dd9db8233a2bf5b7f0b34f6ffe69d4e24cf
TPolan <t.poly@seznam.cz>
2019-01-23 09:02:08 +0100

    long overdue update

---
+++ day 12/chessboard challenge/index.js
@@ -0,0 +1,57 @@
+
+// let ranNum = Math.round (Math.random() * 100) + 100;
+
+// console.log(ranNum);
+
+// const diceRoll = (min, max) => {
+//     let roll = Math.round (Math.random() * (max - min)) + min;
+//     return roll;
+// };
+
+// console.log(diceRoll(8, 7));
+
+// console.log(selleryWithTaxChoice("38%"));
+// const selleryTaxed = amount => amount - (amount /  4);
+
+// console.log(sellery(6,5,4));
+// console.log(selleryTaxed(sellery(4,5,6)));
+
+
+
+
+
+
+
+
+// const sellery = (pay, day, hour) => pay * day * hour;
+// const selleryWithTaxChoice = (amount, tax) => {
+//     let realTax = parseInt(tax) / 100;
+//     return  amount - (amount * realTax);
+// }
+
+// let askPay = prompt("Please enter your hourly pay");
+// let askHour = prompt("Please enter number of hours you work in a day");
+// let askDay = prompt("Please enter number of days you work in a month");
+// let askTax = prompt("Please enter the diserd tax percantage");
+
+// const completeSellary = (askPay,askDay,askHour,askTax) => {
+//     let amount =  sellery(askPay,askDay,askHour)
+//     return  selleryWithTaxChoice(amount, askTax);
+// };
+// let answer = completeSellary(askPay,askDay,askHour,askTax);
+// console.log(answer);
+
+
+
+
+// let askPassword = prompt("Please enter your password");
+// const rightPassword = "sore-thumb-218";
+// const checkPassword = (password) => {
+//     if (password !== rightPassword) {
+//         return alert("Nice Try");
+//     } else {
+//         return alert("Welcome");
+//     }
+// };
+// checkPassword(askPassword);
+
